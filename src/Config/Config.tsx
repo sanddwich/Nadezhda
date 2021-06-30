c8b411c0b875dbd22d6ec235fd3dd064569cd19f
Denis <64834314+sanddwich@users.noreply.github.com>
2021-06-30 14:50:03 +0400

    123213

--- src/Config/Config.tsx
+++ src/Config/Config.tsx
@@ -35,6 +35,21 @@ const Config: Config = {
     '8.jpg',
     '9.jpg',
     '10.jpg',
+    '11.jpg',
+    '12.jpg',
+    '13.jpg',
+    '14.jpg',
+    '15.jpg',
+    '16.jpg',
+    '17.jpg',
+    '18.jpg',
+    '19.jpg',
+    '20.jpg',
+    '21.jpg',
+    '22.jpg',
+    '23.jpg',
+    '24.jpg',
+    '25.jpg',
   ]
 }
 
